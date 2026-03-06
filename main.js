71222e80f4df9865cc6be12a01204f2688327630
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-07 03:06:08 +0700

    feat: complete l02

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive
+const title = "js-react"
+const screens = "Простые, Сложные, Интерактивные"
+const screenPrice = (Math.random() * Math.random() * 1000).toFixed(0)
+const rollback = Math.floor(Math.random() * 100)
+const fullPrice = Math.round(Math.random() * 5000)
+const adaptive = !true
 
-alert('Hello olleH!')
-console.log(title);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
+console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+
+console.log(screens.toLocaleLowerCase().split(""));
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback/100)} `);
