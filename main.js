a62a9281664faeae953a9b726bf283e02e8304ca
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-10 03:13:29 +0700

    feat: complete l03

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-const title = "js-react"
-const screens = "Простые, Сложные, Интерактивные"
-const screenPrice = (Math.random() * Math.random() * 1000).toFixed(0)
-const rollback = Math.floor(Math.random() * 100)
-const fullPrice = Math.round(Math.random() * 5000)
-const adaptive = !true
+const title = prompt("What is your project?")
+const screens = prompt("What types of screens need to develope?")
+const screenPrice = +prompt("How much will this work cost?")
+const adaptive = confirm("Will the site be adaptive?")
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+const user1 = {
+  service: prompt("What additional type of service is required?"),
+  servicePrice: +prompt("How much will it cost?"),
+}
+const user2 = {
+  service: prompt("What additional type of service is required?"),
+  servicePrice: +prompt("How much will it cost?"),
+}
+const rollback = 20 // %
+const fullPrice = screenPrice + user1.servicePrice + user2.servicePrice
+const commision = fullPrice * (rollback/100)
+const servicePercentPrice = Math.ceil(fullPrice - commision)
 
-console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
-console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+console.log(servicePercentPrice);
 
- // В задании не указано на какой массив разделять так что 2 варианта
-console.log(screens.toLocaleLowerCase().split(""));
-console.log(screens.toLocaleLowerCase().split(", "));
-console.log(`Процент отката посреднику за работу ${fullPrice * (rollback/100)} `);
+console.log(`Cost of screen layout ${screenPrice}$`);
+console.log(`Website dev cost ${fullPrice}$`);
+console.log(`Commission to intermediary for work ${commision}$`);
+
+  if (fullPrice > 3000) {
+  console.log("Descount 10%");
+} else if (fullPrice > 1500 && fullPrice <= 3000) {
+  console.log("Descount 5%");
+} else if (fullPrice <= 1500 && fullPrice >= 0) {
+  console.log("No discount is available");
+} else {
+  console.log("Something went wrong");
+}
