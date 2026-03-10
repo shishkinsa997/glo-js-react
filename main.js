18fb56e6fdf7ad691193b29258bdacd1194d4202
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-11 05:15:44 +0700

    feat: complete l04

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
-const title = prompt("What is your project?")
-const screens = prompt("What types of screens need to develope?")
-const screenPrice = +prompt("How much will this work cost?")
-const adaptive = confirm("Will the site be adaptive?")
-
+const title = prompt("What is your project?");
+const screens = prompt("What types of screens need to develope?");
+const screenPrice = +prompt("How much will this work cost?");
+const adaptive = confirm("Will the site be adaptive?");
 const user1 = {
   service: prompt("What additional type of service is required?"),
   servicePrice: +prompt("How much will it cost?"),
-}
+};
 const user2 = {
   service: prompt("What additional type of service is required?"),
   servicePrice: +prompt("How much will it cost?"),
+};
+const getAllServicePrices = function () {
+  return user1.servicePrice + user2.servicePrice;
+};
+
+const allServicePrices = getAllServicePrices();
+const rollback = 20; // %
+const fullPrice = getFullPrice();
+const commision = fullPrice * (rollback / 100);
+const servicePercentPrice = getServicePercentPrices();
+
+const showTypeOf = function (value) {
+  console.log(value, typeof value);
+};
+
+const getRollBackMessage = function (price) {
+  if (price > 3000) {
+    return "Descount 10%";
+  } else if (price > 1500 && price <= 3000) {
+    return "Descount 5%";
+  } else if (price <= 1500 && price >= 0) {
+    return "No discount is available";
+  } else {
+    return "Something went wrong";
+  }
+};
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
 }
-const rollback = 20 // %
-const fullPrice = screenPrice + user1.servicePrice + user2.servicePrice
-const commision = fullPrice * (rollback/100)
-const servicePercentPrice = Math.ceil(fullPrice - commision)
 
-console.log(servicePercentPrice);
-
-console.log(`Cost of screen layout ${screenPrice}$`);
-console.log(`Website dev cost ${fullPrice}$`);
-console.log(`Commission to intermediary for work ${commision}$`);
-
-  if (fullPrice > 3000) {
-  console.log("Descount 10%");
-} else if (fullPrice > 1500 && fullPrice <= 3000) {
-  console.log("Descount 5%");
-} else if (fullPrice <= 1500 && fullPrice >= 0) {
-  console.log("No discount is available");
-} else {
-  console.log("Something went wrong");
+const getTitle = function (title) {
+  title = title.trim().toLowerCase();
+  return title[0].toUpperCase() + title.slice(1);
+};
+function getServicePercentPrices() {
+  return Math.ceil(fullPrice - commision);
 }
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(screens);
+
+console.log(getRollBackMessage(fullPrice));
+console.log("Cost of work: ", getServicePercentPrices());
