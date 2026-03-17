1ddfd701345ed90a1658d630ac7ea4d7e1fda559
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-18 04:00:56 +0700

    feat: complete l06 extra

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let n = 46;
+let guess = Math.floor(Math.random() * 100);
+let attempts = 10;
 
 const getNumber = function () {
   return prompt("Guess a number from 1 to 100");
 };
 
 const guessNumber = function () {
+  if (attempts <= 0) {
+    if (confirm("Attempts are over, want to play again?")) {
+      attempts = 10
+      guessNumber();
+    }
+    return;
+  }
   number = getNumber();
   console.log(number);
 
@@ -14,18 +22,26 @@ const guessNumber = function () {
   }
   if (isNaN(number) || !number.trim()) {
     alert("Not a number");
-    number = guessNumber();
+    return guessNumber();
   }
 
-  if (+number === n) {
-    alert("Congrats!");
-  } else if (+number > n) {
-    alert("Too big");
-    number = guessNumber();
-  } else if (+number < n) {
-    alert("Too small");
-    number = guessNumber();
+  if (+number === guess) {
+    if (confirm("Congrsts!. Want to play again?")) {
+      attempts = 10
+      guessNumber();
+    }
+    return;
   }
+
+  attempts--;
+
+  if (+number > guess) {
+    alert("Too big. Attempts left: " + attempts);
+  } else if (+number < guess) {
+    alert("Too small. Attempts left: " + attempts);
+  }
+
+  guessNumber();
 };
 
 guessNumber();
