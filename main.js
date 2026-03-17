ab39a1be93dc6e069a63bd0bc468131f479c55bf
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-18 01:45:35 +0700

    feat: complete l05

--- main.js
+++ main.js
@@ -1,27 +1,76 @@
-const title = prompt("What is your project?");
-const screens = prompt("What types of screens need to develope?");
-const screenPrice = +prompt("How much will this work cost?");
-const adaptive = confirm("Will the site be adaptive?");
-const user1 = {
-  service: prompt("What additional type of service is required?"),
-  servicePrice: +prompt("How much will it cost?"),
-};
-const user2 = {
-  service: prompt("What additional type of service is required?"),
-  servicePrice: +prompt("How much will it cost?"),
-};
-const getAllServicePrices = function () {
-  return user1.servicePrice + user2.servicePrice;
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+
+let rollback = 20; // %
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let service1;
+let service2;
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(parseFloat(num));
 };
 
-const allServicePrices = getAllServicePrices();
-const rollback = 20; // %
-const fullPrice = getFullPrice();
-const commision = fullPrice * (rollback / 100);
-const servicePercentPrice = getServicePercentPrices();
+const asking = function () {
+  title = prompt("What is your project?", "Project");
+  screens = prompt(
+    "What types of screens need to develope?",
+    "Simple, Complex",
+  );
+
+  do {
+    screenPrice = parseFloat(prompt("How much will this work cost?", 1200));
+  } while (!isNumber(screenPrice));
+
+  screenPrice = parseFloat(screenPrice);
+
+  adaptive = confirm("Will the site be adaptive?");
+};
+
+const getAllServicePrices = function () {
+  let servicePrice;
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt(
+        "What additional type of service is required?",
+        "Packaging",
+      );
+    } else if (i === 1) {
+      service2 = prompt(
+        "What additional type of service is required?",
+        "Shipping",
+      );
+    }
+
+    do {
+      servicePrice = parseFloat(prompt("How much will it cost?", 1500));
+    } while (!isNumber(servicePrice));
+
+    sum += servicePrice;
+  }
+
+  return sum;
+};
 
 const showTypeOf = function (value) {
   console.log(value, typeof value);
+};
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
+}
+
+function getServicePercentPrices() {
+  return Math.ceil(fullPrice - fullPrice * (rollback / 100));
+}
+
+const getTitle = function (title) {
+  title = title.trim().toLowerCase();
+  return title[0].toUpperCase() + title.slice(1);
 };
 
 const getRollBackMessage = function (price) {
@@ -36,23 +85,19 @@ const getRollBackMessage = function (price) {
   }
 };
 
-function getFullPrice() {
-  return screenPrice + allServicePrices;
-}
+asking();
 
-const getTitle = function (title) {
-  title = title.trim().toLowerCase();
-  return title[0].toUpperCase() + title.slice(1);
-};
-function getServicePercentPrices() {
-  return Math.ceil(fullPrice - commision);
-}
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle(title);
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
-console.log(screens);
-
 console.log(getRollBackMessage(fullPrice));
-console.log("Cost of work: ", getServicePercentPrices());
+console.log(screens.length);
+
+console.log("allServicePrices: ", allServicePrices);
+console.log("servicePercentPrice: ", servicePercentPrice);
