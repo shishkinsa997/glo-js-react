a160e614c8c25e6ae89c803b26129e48beeadcac
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-28 17:16:10 +0500

    feat: complete l16

--- main.js
+++ main.js
@@ -1,74 +1,18 @@
-const DomElement = function (selector, height, width, bg, fontSize, text) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
+class First {
+  hello() {
+    console.log("Привет я метод родителя");
+  }
+}
 
-  this.create = function () {
-    let el;
+class Second extends First {
+  constructor() {
+    super();
+  }
+  hello() {
+    super.hello()
+    console.log("А я наследуемый метод");
+  }
+}
 
-    if (this.selector.startsWith(".")) {
-      el = document.createElement("div");
-      el.classList.add(this.selector.slice(1));
-    }
-    if (this.selector.startsWith("#")) {
-      el = document.createElement("p");
-      el.id = this.selector.slice(1);
-    }
-    el.textContent = text;
-    el.style.cssText = `
-        height: ${this.height}px;
-        width: ${this.width}px;
-        background: ${this.bg};
-        font-size: ${this.fontSize};
-        position: absolute;
-        top: 50dvh;
-        left: 50dvw;
-    `;
-    this.el = el;
-    return el;
-  };
-};
-
-DomElement.prototype.move = function () {
-  document.addEventListener("keydown", (e) => {
-    const key = e.key;
-    const step = 10;
-
-    if (key === "ArrowUp") {
-      this.el.style.top = this.el.offsetTop - step + "px";
-      console.log("^");
-    }
-
-    if (key === "ArrowDown") {
-      this.el.style.top = this.el.offsetTop + step + "px";
-      console.log("v");
-    }
-
-    if (key === "ArrowLeft") {
-      this.el.style.left = this.el.offsetLeft - step + "px";
-      console.log("<");
-    }
-
-    if (key === "ArrowRight") {
-      this.el.style.left = this.el.offsetLeft + step + "px";
-      console.log(">");
-    }
-  });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  const blockObj = new DomElement(
-    (selector = ".block"),
-    (height = "100"),
-    (width = "100"),
-    (bg = "red"),
-    (fontSize = "16"),
-  );
-
-  blockObj.move();
-  const block = blockObj.create();
-
-  document.body.append(block);
-});
+const second = new Second
+second.hello()
